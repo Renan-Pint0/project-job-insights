b16f3c306340f07ab4c115400040a9aadf86b27c
Renan Pinto <renanrnpinto@gmail.com>
2023-03-27 16:57:13 -0300

    Adiciona as funções matches_salary_range e filter_by_salary_range

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -33,47 +33,29 @@ def get_min_salary(path: str) -> int:
 
 
 def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
-    """Checks if a given salary is in the salary range of a given job
-
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
-
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
-
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
-    raise NotImplementedError
+    try:
+        min_salary = int(job["min_salary"])
+        max_salary = int(job["max_salary"])
+        if int(min_salary) > int(max_salary):
+            raise (ValueError)
+        elif int(min_salary) <= int(salary) <= int(max_salary):
+            return True
+        else:
+            return False
+    except (TypeError, KeyError):
+        raise ValueError
 
 
 def filter_by_salary_range(
     jobs: List[dict],
     salary: Union[str, int]
 ) -> List[Dict]:
-    """Filters a list of jobs by salary range
-
-    Parameters
-    ----------
-    jobs : list
-        The jobs to be filtered
-    salary : int
-        The salary to be used as filter
-
-    Returns
-    -------
-    list
-        Jobs whose salary range contains `salary`
-    """
-    raise NotImplementedError
+    job_list = []
+    for job in jobs:
+        try:
+            job_salary = matches_salary_range(job, salary)
+            if job_salary is True:
+                job_list.append(job)
+        except ValueError:
+            print("Invalid Salary")
+    return job_list
