c30bac37cfeaf8ecf8d8ad7a0fbef28afe6ab920
Renan Pinto <renanrnpinto@gmail.com>
2023-03-27 20:02:31 -0300

    Adiciona test_brazilian_jobs

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,7 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    result = {"title": "Maquinista", "salary": "2000", "type": "trainee"}
+    path = "tests/mocks/brazilians_jobs.csv"
+    assert read_brazilian_file(path)[0] == result
